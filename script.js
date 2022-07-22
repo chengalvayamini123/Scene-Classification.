b2347d64543e986aec5ad9ce8361ce9d05642ce2
chengalvayamini123 <109744260+chengalvayamini123@users.noreply.github.com>
2022-07-22 17:06:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:"https://www.nyckel.com/v1/functions/w6x6xgd22747jqr5/invoke",
+        url:'https://www.nyckel.com/v1/functions/w6x6xgd22747jqr5/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
